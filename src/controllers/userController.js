497dbf4bc382b586e9a634fba3d6ef685183e3fd
Danimell061 <dani9ct@gmail.com>
2025-09-02 19:48:58 -0300

    Iniciando Projeto

---
+++ src/controllers/userController.js
@@ -0,0 +1,14 @@
+export const createUser = (req, res) => {
+
+    res.status(200).json({message : 'Deu bom'})
+}
+
+export const getAllUsers = (req, res) => {
+
+    res.status(200).json({message : 'Deu bom'})
+}
+
+export const deleteUser = (req, res) => {
+
+    res.status(200).json({message : 'Deu bom'})
+}
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { createUser, getAllUsers, deleteUser } from './controllers/userController'
+
+const router = express.Router()
+
+router.post('/cadastro', createUser)
+router.get('/todos', getAllUsers)
+router.delete('/delete', deleteUser)
