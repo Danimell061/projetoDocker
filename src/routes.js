278d01142df7cb3787df7fab32510aedb5e0f5fb
Danimell061 <dani9ct@gmail.com>
2025-09-02 21:51:09 -0300

    Iniciando Configurações de dependencias para manipulação do projeto

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { createUser, getAllUsers, deleteUser } from './controllers/userController'
+import { createUser, getAllUsers, deleteUser } from './controllers/userController.js'
 
 const router = express.Router()
 
 router.post('/cadastro', createUser)
 router.get('/todos', getAllUsers)
 router.delete('/delete', deleteUser)
+
+export default router
